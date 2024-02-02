62f56ab7f8e0d6fe205684179a7222e1edd85e2c
Alexander Belov <alexander.belov@zyfra.com>
2024-02-02 23:35:15 +0300

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,9 +12,11 @@ variable "yc_folder_id" {
 
 variable "yc_zones" {
   type = list(string)
+  default = ["ru-central1-a", "ru-central1-b", "ru-central1-c"]
 }
 
 variable "yc_subnets" {
   type = list(list(string))
+  default = [["192.168.1.0/24"], ["192.168.2.0/24"], ["192.168.3.0/24"]]
 }
 
